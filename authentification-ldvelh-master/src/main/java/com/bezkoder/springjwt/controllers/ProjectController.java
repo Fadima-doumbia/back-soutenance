5546aa8ad302afad1a4226a0fed9b6b7445b33d5
Fadima-doumbia <doumbia.fari@gmail.com>
2021-12-28 18:19:31 +0100

    project

--- authentification-ldvelh-master/src/main/java/com/bezkoder/springjwt/controllers/ProjectController.java
+++ authentification-ldvelh-master/src/main/java/com/bezkoder/springjwt/controllers/ProjectController.java
@@ -1,17 +1,14 @@
 package com.bezkoder.springjwt.controllers;
 
 import com.bezkoder.springjwt.dto.ProjectDto;
-import com.bezkoder.springjwt.dto.UserUpdateDto;
 import com.bezkoder.springjwt.models.Project;
 import com.bezkoder.springjwt.models.User;
 import com.bezkoder.springjwt.payload.request.SearchProjectRequest;
-import com.bezkoder.springjwt.repository.RoleRepository;
 import com.bezkoder.springjwt.repository.ProjectRepository;
 import com.bezkoder.springjwt.repository.UserRepository;
 import com.bezkoder.springjwt.security.services.ProjectService;
 import com.bezkoder.springjwt.security.services.impl.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -31,24 +28,18 @@ public class ProjectController {
     UserRepository userRepository;
     @Autowired
     ProjectRepository p;
-    @Autowired
-    RoleRepository roleRepository;
 
 
- /*   @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
     @PostMapping("/{idUser}")
     public User createProjet(@PathVariable("idUser") final Long idUser, @RequestBody ProjectDto projectDto){
-        return projetService.saveProject(projectDto, idUser);
-    }*/
-     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
-     @PostMapping("/{idUser}")
-     public User createProjet(@PathVariable("idUser") final Long idUser, @RequestBody ProjectDto projectDto){
-         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-         String username = auth.getName();
-         Optional <User> optionalUser = userRepository.findByUsername(username);
-         Long id = optionalUser.get().getId();
-         return projetService.saveProject(projectDto, id);
-     }
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        Optional <User> optionalUser = userRepository.findByUsername(username);
+        Long id = optionalUser.get().getId();
+        return projetService.saveProject(projectDto, id);
+    }
+
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR', 'ROLE_INVESTISSEUR')")
     @GetMapping("")
     public Iterable<Project> getProjects(){
@@ -65,141 +56,84 @@ public class ProjectController {
             return null;
         }
     }
-//
-//            @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
-//            @PostMapping("/{idUser}")
-//            public User createProjet ( @PathVariable("idUser") final Long idUser, @RequestBody ProjectDto projectDto){
-//                Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//                String username = auth.getName();
-//                Optional<User> optionalUser = userRepository.findByUsername(username);
-//                Long id = optionalUser.get().getId();
-//                return projetService.saveProject(projectDto, id);
-//            }
-//
-//            @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR', 'ROLE_INVESTISSEUR')")
-//            @GetMapping("")
-//            public Iterable<Project> getProjects () {
-//                return projetService.getProjects();
-//            }
 
-//            @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR', 'ROLE_INVESTISSEUR')")
-//            @GetMapping("/{id}")
-//            public Project getProjet ( @PathVariable("id") final Long id){
-//                Optional<Project> projet = projetService.getProject(id);
-//                if (projet.isPresent()) {
-//                    return projet.get();
-//                } else {
-//                    return null;
-//                }
-//            }
+    @PostMapping("/searchProject")
+    public List<Project> searchProjectByName(@RequestBody SearchProjectRequest searchProjectRequest){
+        List<Project> project = null;
+        if (searchProjectRequest.getName().isEmpty()){
+            project = projetService.getProjects();
+        }else{
+            project=projetService.searchProjectByName(searchProjectRequest);
+        }
+        return project;
+    }
 
-            @PostMapping("/searchProject")
-            public List<Project> searchProjectByName (@RequestBody SearchProjectRequest searchProjectRequest){
-                List<Project> project = null;
-                if (searchProjectRequest.getName().isEmpty()) {
-                    project = projetService.getProjects();
-                } else {
-                    project = projetService.searchProjectByName(searchProjectRequest);
-                }
-                return project;
-            }
+    @PreAuthorize("hasAnyRole('ROLE_ENTREPRENEUR', 'ROLE_ENTREPRENEUR')")
+    @PutMapping("")
+    public ResponseEntity<String> updateProjet(@RequestBody() ProjectDto projectDto, Authentication authentication) {
+        final UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
+        Optional <User> userOptional = userRepository.findById(user.getId());
 
-            @PreAuthorize("hasAnyRole('ROLE_ENTREPRENEUR', 'ROLE_ENTREPRENEUR')")
-            @PutMapping("")
-            public ResponseEntity<String> updateProjet (@RequestBody() ProjectDto projectDto, Authentication
-            authentication){
-                final UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
-                Optional<User> userOptional = userRepository.findById(user.getId());
+        if(projectDto.getUserId() == userOptional.get().getId()){
+            userOptional.get().getProjects().remove(projectDto);
+            projetService.updateProject(projectDto);
+            return new ResponseEntity<String>("update ok",HttpStatus.OK);
+        }else {
+            return new ResponseEntity<String>("Ce Projet ne vous appartient pas; Vous ne pourrez pas le modifier", HttpStatus.FORBIDDEN);
+        }
+    }
 
-                if (projectDto.getUserId() == userOptional.get().getId()) {
-                    userOptional.get().getProjects().remove(projectDto);
-                    projetService.updateProject(projectDto);
-                    return new ResponseEntity<String>("update ok", HttpStatus.OK);
-                } else {
-                    return new ResponseEntity<String>("Ce Projet ne vous appartient pas; Vous ne pourrez pas le modifier", HttpStatus.FORBIDDEN);
-                }
-            }
-
-            @PreAuthorize("hasAnyRole('ROLE_ENTREPRENEUR', 'ROLE_ENTREPRENEUR')")
-            @DeleteMapping("/{id}")
-            public ResponseEntity<String> deleteProjet ( @PathVariable("id") final Long id, Authentication
-            authentication){
-                final UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
-                Optional<User> userOptional = userRepository.findById(user.getId());
-                Optional<Project> optionalProject = projetService.getProject(id);
-                if (optionalProject.get().getUserId() == userOptional.get().getId()) {
-                    userOptional.get().getProjects().remove(optionalProject);
-                    projetService.projectDelete(optionalProject.get().getId());
-                    return new ResponseEntity<String>("delete ok", HttpStatus.OK);
-                } else {
-                    return new ResponseEntity<String>("Ce Projet ne vous appartient pas; Vous ne pourrez pas le supprimer", HttpStatus.FORBIDDEN);
-                }
-            }
+    @PreAuthorize("hasAnyRole('ROLE_ENTREPRENEUR', 'ROLE_ENTREPRENEUR')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProjet(@PathVariable("id") final Long id, Authentication authentication) {
+        final UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
+        Optional <User> userOptional = userRepository.findById(user.getId());
+        Optional <Project> optionalProject = projetService.getProject(id);
+        if(optionalProject.get().getUserId() == userOptional.get().getId()){
+            userOptional.get().getProjects().remove(optionalProject);
+            projetService.projectDelete(optionalProject.get().getId());
+            return new ResponseEntity<String>("delete ok",HttpStatus.OK);
+        }else {
+            return new ResponseEntity<String>("Ce Projet ne vous appartient pas; Vous ne pourrez pas le supprimer", HttpStatus.FORBIDDEN);
+        }
+    }
 
 
-            @PreAuthorize("hasRole('ADMIN')")
-            @PutMapping("/admin")
-            public Project updateUProject (@RequestBody ProjectDto projectDto){
-                return projetService.adminUpdateProject(projectDto);
-            }
-
-            @PreAuthorize("hasRole('ADMIN')")
-            @DeleteMapping("/admin/{id}")
-            public void deleteProjetAdmin ( @PathVariable("id") final Long id){
-                projetService.adminDeleteProject(id);
-            }
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/admin")
+    public Project updateUProject(@RequestBody ProjectDto projectDto){
+        return projetService.adminUpdateProject(projectDto);
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/admin/{id}")
+    public void deleteProjetAdmin(@PathVariable("id") final Long id) {
+        projetService.adminDeleteProject(id);
+    }
 //    }
 
-/*    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
     @PutMapping("")
     public Project updateProjet(@RequestBody ProjectDto projectDto){
         return projetService.updateProject(projectDto);
-    }*/
+    }
 
-//cette methode fonctionne mais ne renvoi pas de forbidden
-/*            @PutMapping("")
-            public void updateProject (@RequestBody ProjectDto projectDto){
-                Optional<User> optionalUser = userRepository.findById(projectDto.getUserId());
-                projectDto.setId(projectDto.getId());
-                projectDto.setUserId(projectDto.getUserId());
-                Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-                String username = auth.getName();
-                Optional<User> optionalUserr = userRepository.findByUsername(username);
-                Long id = optionalUser.get().getId();
-                Long userId = optionalUserr.get().getId();
-                if (id == userId) {
-                    projetService.updateProject(projectDto);
-                }
-            }*/
-
-/*    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
     @DeleteMapping("/{id}")
     public void deleteProjet(@PathVariable("id") final Long id) {
         projetService.projectDelete(id);
-    }*/
-
-/*            @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
-            @DeleteMapping("/{id}")
-            public void deleteProjet ( @PathVariable("id") final Long id){
-                Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-                String username = auth.getName();
-                Optional<User> optionalUser = userRepository.findByUsername(username);
-                Long userid = optionalUser.get().getId();
-                projetService.projectDelete(userid, id);
-            }*/
+    }
 
 
-      /*      @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
-            @DeleteMapping("/delete/{id}")
-            public void deleteProject ( @PathVariable("id") final Long id){
-                String authentication = Authentication.class.getName();
-                Optional<User> optionalUser = userRepository.findByUsername(authentication);
-                Long userId = optionalUser.get().getId();
-                Optional<Project> projectOptional = projetService.getProject(id);
-                Long projectUserId = projectOptional.get().getUserId();
-                if (userId == projectUserId) {
-                    projetService.projectDelete(id);
-                }
-            }*/
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ENTREPRENEUR')")
+    @DeleteMapping("/delete/{id}")
+    public void deleteProject(@PathVariable("id") final Long id) {
+        String authentication = Authentication.class.getName();
+        Optional <User> optionalUser = userRepository.findByUsername(authentication);
+        Long userId = optionalUser.get().getId();
+        Optional <Project> projectOptional = projetService.getProject(id);
+        Long projectUserId = projectOptional.get().getUserId();
+        if(userId == projectUserId){
+            projetService.projectDelete(id);
+        }
+    }
 }
-
--- authentification-ldvelh-master/src/main/java/com/bezkoder/springjwt/security/services/ProjectService.java
+++ authentification-ldvelh-master/src/main/java/com/bezkoder/springjwt/security/services/ProjectService.java
@@ -46,37 +46,12 @@ public class ProjectService {
         userRepository.save(userOptional);
 
     }
-    public void projectDelete(Long userid, Long id) {
-        Project projetOptional = projectRepository.findById(id).get();
-        User userOptional = userRepository.findById(userid).get();
-        userOptional.getProjects().remove(projetOptional);
-        userRepository.save(userOptional);
-    }
-//    public Project updateProject(ProjectDto projectDto){
-//        Project project = modelMapper.map(projectDto, Project.class);
-//        return projectRepository.save(project);
-//    }
-
 
     public Project updateProject(ProjectDto projectDto){
+
         Project project = modelMapper.map(projectDto, Project.class);
         return projectRepository.save(project);
     }
-//    public void updateProject(ProjectDto projectDto){
-//        Project project = modelMapper.map(projectDto, Project.class);
-//        projectRepository.save(project);
-//    }
-//    public Project updateProject(ProjectDto projectDto, Long id){
-//        Long idUser = projectDto.getUserId();
-//        if (id==idUser){
-//            Project project = modelMapper.map(projectDto, Project.class);
-//            return projectRepository.save(project);
-//        }else{
-//             return HttpStatus.FORBIDDEN;
-//        }
-//        return null;
-//
-//    }
 
     public User saveProject(ProjectDto projectDto, Long idUser){
         Optional<User> userOptional = userRepository.findById(idUser);
